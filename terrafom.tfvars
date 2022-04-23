ebfb55c4b64d5536dfc16a500f08b78b4d875283
sandhyav8 <sandhyamanchala555@gmail.com>
2022-04-23 13:13:45 +0530

    basecode 1

---
+++ terrafom.tfvars
@@ -0,0 +1,5 @@
+resource_group_name = "firstRG"
+location_name = "australiaeast"
+virtual_network_name = "vnet1"
+vnet_addr = ["10.2.0.0/16"]
+subnet_name = "subnet1"
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "resource_group_name" {}
+variable "location_name" {}
+variable "virtual_network_name" {}
+variable "vnet_addr" {}
+variable "subnet_name" {}
